e9851ad9c11cbc6d7caeb10e5ec2e9ef8555405d
Revirdi <ardwiyanrevirdi99@gmail.com>
2023-01-18 17:53:34 +0700

    Test Airpaz No.5

---
+++ phpTest/AirpazNo5.php
@@ -0,0 +1,12 @@
+<?php
+
+function PrintStar(){
+    for($i = 15; $i >= 0; $i-=2){
+        for($j = 0; $j < $i; $j++ ){
+            echo "* ";
+        }
+        echo "\n";
+    }
+}
+
+PrintStar();
