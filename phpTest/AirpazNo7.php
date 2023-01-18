39fc5406ac707a79d0781359fe4ccb3930697795
Revirdi <ardwiyanrevirdi99@gmail.com>
2023-01-18 19:09:53 +0700

    Airpaz Test No.7

---
+++ phpTest/AirpazNo7.php
@@ -0,0 +1,24 @@
+<?php
+
+function DifferentNum($arr1, $arr2){
+    $temp = [];
+    $result = [];
+    foreach($arr1 as $value){
+        $temp[$value] = $value;
+    }
+
+    foreach($arr2 as $value){
+        if(!isset($temp[$value])){
+            $temp[$value] = $value;
+            continue;
+        } 
+        if(isset($temp[$value])){
+            unset($temp[$value]);
+        }
+    }
+    return array_values($temp);
+}
+
+$arr1 = [1,2,3,4,5];
+$arr2 = [1,3,4,5,6];
+var_dump(DifferentNum($arr1, $arr2));
